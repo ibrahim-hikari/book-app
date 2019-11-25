b47e9b5d77a0794f0598fed376987408e87ff5c9
ibraim-Ajarmeh <ajarmeh1997@gmail.com>
2019-11-25 13:03:22 +0200

    got the information needed, still the render after search

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ server.use(express.urlencoded({extended:true}));
 server.set('view engine', 'ejs');
 
 server.get('/', renderForm);
-server.post('/find', findBook);
+server.post('/searches', bookHandler);
 
 
 
@@ -21,7 +21,30 @@ function renderForm(req,res) {
     res.render('pages/index')
 }
 
+function bookHandler(req, res) {
+    // Query String = ?a=b&c=d
+    findBook(req.query.data)
+        .then(bookData => res.status(200).json(bookData));
+
+}
+
 function findBook(req,res) {
-    const url = ''
+    const url = 'https://www.googleapis.com/books/v1/volumes?q=search+terms'
+    return superagent.get(url)
+        .then(data =>{
+            let books = data.body;
+            return books.items.map((stuff)=>{
+                return new Book(stuff);
+            });
+        });
+}
+
+
+function Book(data) {
+    this.authors = data.volumeInfo.authors[0]
+    this.title = data.volumeInfo.title
+    this.id = data.id
+    this.description = data.volumeInfo.description
+    this.image = data.volumeInfo.imageLinks
 }
 server.listen(PORT, () => { console.log(`Hello form ${PORT}`); })
