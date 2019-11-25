4a7bf4d58bcda5fae1519c9ec3a91f7f75e3c0a0
ibraim-Ajarmeh <ajarmeh1997@gmail.com>
2019-11-25 13:47:42 +0200

    still in the same area

--- server.js
+++ server.js
@@ -8,32 +8,36 @@ const PORT = process.env.PORT || 3000;
 const server = express();
 
 server.use(express.static('./public'));
-server.use(express.urlencoded({extended:true}));
+server.use(express.urlencoded({ extended: true }));
 server.set('view engine', 'ejs');
 
 server.get('/', renderForm);
-server.post('/searches', bookHandler);
+server.get('/searches', bookHandler);
+
+server.get('/test', (req, res) => {
+    // res.render('pages/searches/show')
+    res.redirect('http://www.google.com')
+})
 
 
 
-
-function renderForm(req,res) {
+function renderForm(req, res) {
     res.render('pages/index')
 }
 
 function bookHandler(req, res) {
-    // Query String = ?a=b&c=d
     findBook(req.query.data)
-        .then(bookData => res.status(200).json(bookData));
-
+        .then(bookData =>  res.status(200).json(bookData) );
+       
+        // res.render('pages/searches/show', bookData)
 }
 
-function findBook(req,res) {
+function findBook(req, res) {
     const url = 'https://www.googleapis.com/books/v1/volumes?q=search+terms'
     return superagent.get(url)
-        .then(data =>{
+        .then(data => {
             let books = data.body;
-            return books.items.map((stuff)=>{
+            return books.items.map((stuff) => {
                 return new Book(stuff);
             });
         });
@@ -43,7 +47,7 @@ function findBook(req,res) {
 function Book(data) {
     this.authors = data.volumeInfo.authors[0]
     this.title = data.volumeInfo.title
-    this.id = data.id
+    this.ISBN = data.volumeInfo.industryIdentifiers
     this.description = data.volumeInfo.description
     this.image = data.volumeInfo.imageLinks
 }
