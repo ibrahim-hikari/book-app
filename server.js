fe2c4b8bf390b81a08a17a193dbecba522b3db1f
ibraim-Ajarmeh <ajarmeh1997@gmail.com>
2019-11-25 20:17:04 +0200

    still doing the lab assignment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 require('dotenv').config();
 const express = require('express');
-const superagent = require('superagent')
+const superagent = require('superagent');
 
 const PORT = process.env.PORT || 3000;
 const server = express();
@@ -12,11 +12,11 @@ server.use(express.urlencoded({ extended: true }));
 server.set('view engine', 'ejs');
 
 server.get('/', renderForm);
-server.get('/searches', bookHandler);
+server.get('/searches', findBook);
 
 server.get('/test', (req, res) => {
-    // res.render('pages/searches/show')
-    res.redirect('http://www.google.com')
+    res.render('pages/searches/show')
+    // res.redirect('http://www.google.com')
 })
 
 
@@ -25,22 +25,19 @@ function renderForm(req, res) {
     res.render('pages/index')
 }
 
-function bookHandler(req, res) {
-    findBook(req.query.data)
-        .then(bookData =>  res.status(200).json(bookData) );
-       
-        // res.render('pages/searches/show', bookData)
-}
 
 function findBook(req, res) {
-    const url = 'https://www.googleapis.com/books/v1/volumes?q=search+terms'
+    let searchBy = authors;
+    let words = peanuts;
+    const url = `https://www.googleapis.com/books/v1/volumes?q=in${searchBy}:${words}`
     return superagent.get(url)
         .then(data => {
             let books = data.body;
             return books.items.map((stuff) => {
                 return new Book(stuff);
             });
-        });
+        })
+        .then(voala => res.render('pages/searches/show', {asd:voala}));
 }
 
 
